194e28b0dad3578718ee23f339d0defd7a1f978d
ioangheraszim <gijohny17@gmail.com>
2025-09-18 16:59:15 +0300

    test: ran secondary test successfully

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,13 +1,12 @@
-import { render, screen } from "@testing-library/react";
+// App.test.jsx
 
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-describe("App", () => {
-  it("renders headline", () => {
-    render(<App title="React" />);
-
-    screen.debug();
-
-    // check if App components renders headline
+describe("App component", () => {
+  it("renders correct heading", () => {
+    render(<App />);
+    expect(screen.getByRole("heading").textContent).toMatch(/our first test/i);
   });
 });
